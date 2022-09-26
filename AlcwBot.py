f8483371328f83459e45d512dfd1e98aae6be0ea
LCW <2b04.2036348.liawchoonwei@gmail.com>
2022-09-26 18:56:15 +0800

    .

--- AlcwBot.py
+++ AlcwBot.py
@@ -7,12 +7,16 @@ db = pd.read_csv('db.csv')
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+# @bot.event
+# async def on_message(message):
+# 	if message.content.lower() == "hello" or message.content.lower() == "hi":
+# 		await message.channel.send(f"hey {message.author},{db}")
+
+members_list = discord.Guild().members
 @bot.event
 async def on_message(message):
 	if message.content.lower() == "hello" or message.content.lower() == "hi":
-		await message.channel.send(f"hey {message.author},{db}")
-
-
+		await message.channel.send(f"hey {message.author},{members_list}")
 
 
 bot.run("MTAyMzgzMDk1NzgxMzQxNTk4Nw.GrIYd6._Te5TGLFKXXfECO1sTce8kS85djLCW_tTBBB6E")
