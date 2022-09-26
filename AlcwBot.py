db6476c7c0076f3f0364237b6a74279a9306c781
LCW <2b04.2036348.liawchoonwei@gmail.com>
2022-09-26 18:46:36 +0800

    .

--- AlcwBot.py
+++ AlcwBot.py
@@ -1,13 +1,17 @@
 import discord
+import pandas as pd
 from discord.ext import commands
 
+
+db = pd.read_csv('db.csv')
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @bot.event
 async def on_message(message):
 	if message.content.lower() == "hello" or message.content.lower() == "hi":
-		await message.channel.send(f"hey {message.author}")
+		await message.channel.send(f"hey {message.author},{pd}")
+
 
 
 
